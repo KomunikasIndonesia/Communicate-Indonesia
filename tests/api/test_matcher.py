6c39120a2e3b39319309bf7fd576ae5059aac1f7
girikuncoro <girikuncoro@gmail.com>
2015-07-28 22:00:19 -0400

    added editdistance to dependencies and added test script for matcher

---
+++ tests/api/test_matcher.py
@@ -0,0 +1,34 @@
+import unittest
+from app.api.matcher import match
+
+
+class MatchTest(unittest.TestCase):
+
+    def test_char_typo(self):
+        cmd = 'jual'
+        usrinput = 'jaul'
+        self.assertTrue(match(usrinput, cmd))
+
+    def test_more_char(self):
+        cmd = 'panen'
+        usrinput = 'paanen'
+        self.assertTrue(match(usrinput, cmd))
+
+    def test_more_chars(self):
+        cmd = 'lihat'
+        usrinput = 'liihaatt'
+        self.assertTrue(match(usrinput, cmd))
+
+    def test_with_defined_limit(self):
+        cmd = 'kirim'
+        usrinput = 'kirimmm'
+        self.assertFalse(match(usrinput, cmd, limit=1))
+
+    def test_with_less_char(self):
+        cmd = 'tanam'
+        usrinput = 'tnam'
+        self.assertTrue(match(usrinput, cmd))
+
+
+if __name__ == '__main__':
+    unittest.main()
