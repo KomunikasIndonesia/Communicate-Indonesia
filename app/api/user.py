7b2bfc0e45b4923ab5bf6444f50cbe4625de2f44
girikuncoro <girikuncoro@gmail.com>
2015-07-30 00:52:48 -0400

    Init commit for user API

--- app/api/user.py
+++ app/api/user.py
@@ -1,74 +1,66 @@
 from flask import Flask, jsonify, request
+from google.appengine.ext import ndb
 import time
 
 app = Flask(__name__)
 
-# To be done:
-# - This code is not tested
-# - Add authentication
-# - Add yaml config
-# - Required/optional validation
-# - Fetching get response by phone number
-# - Add test script for insert/retrieve API
-# - Replace dummy with real data/function
-# - Generate epoch time for ts_created/updated not required on insert()?
-# - Generated id on insert()?
-
-# Dummy data
-USERS = [{
-    'id': 'ci1',
-    'role': 'hutan_biru',
-    'phone_number': '1234567',
-    'first_name': 'Thomas',
-    'last_name': None,
-    'ts_created': 1438141613370,
-    'ts_updated': 1438141613370
-},
-{
-    'id': 'ci2',
-    'role': 'farmers',
-    'phone_number': '531531',
-    'first_name': 'Siti',
-    'last_name': 'Nurbaya',
-    'ts_created': 1438141741970,
-    'ts_updated': 1438141741970
-},
-{
-    'id': 'ci3',
-    'role': 'hutan_biru',
-    'phone_number': '23452345',
-    'first_name': 'Ratna',
-    'last_name': 'Fadilah',
-    'ts_created': 1438141750730,
-    'ts_updated': 1438141750730
-}]
+ROLES = ['hutan_biru', 'farmer']
 
 
-@app.route('v1/users', methods=['PUT'])
+class User(ndb.Model):
+    id = ndb.StringProperty(required=True)
+    role = ndb.StringProperty(required=True, choices=set(ROLES))
+    phone_number = ndb.StringProperty(required=True)
+    first_name = ndb.StringProperty(required=True)
+    last_name = ndb.StringProperty()
+    ts_created = ndb.DateTimeProperty(auto_now_add=True)
+    ts_updated = ndb.DateTimeProperty(auto_now=True)
+
+
+@app.route('/v1/users', methods=['PUT'])
 def insert():
-    data = {
-        'role': request.values.get('role'),
-        'phone_number': request.values.get('phone_number'),
-        'first_name': request.values.get('first_name'),
-        'last_name': request.values.get('last_name') or None
-    }
+    try:
+        req = request.get_json(force=True)
 
-    # Store data
-    # insert_funct(data)
-    msg = 'Successful PUT request'
+        new = User()
+        new = User(id = req['phone_number'],
+                   role = req['role'],
+                   phone_number = req['phone_number'],
+                   first_name = req['first_name'])
 
-    return jsonify({'code': 201, 'response': msg})
+        if 'last_name' in req:
+            new.last_name = req['last_name']
+
+        new.key = ndb.Key(User, req['phone_number'])
+        new.put()
+
+        msg = 'Successful PUT request'
+
+    except Exception:
+        msg = 'Bad PUT request'
+
+    return jsonify({'response': msg})
 
 
-@app.route('v1/users/<userid>', methods=['GET'])
+@app.route('/v1/users/<userid>', methods=['GET'])
 def retrieve(userid):
-    # Getting dummy data
-    users = USERS
+    resp = {'result': {}}
 
-    response = None
+    try:
+        user_key = ndb.Key(User, userid)
+        data = user_key.get()
 
-    for user in users:
-        if userid in user['id']:
-            response = user
+        resp['result'] = {
+            'id': data.id,
+            'role': data.role,
+            'phone_number': data.phone_number,
+            'first_name': data.first_name,
+            'last_name': data.last_name,
+            'ts_created': time.mktime(data.ts_created.timetuple()) * 1000,
+            'ts_updated': time.mktime(data.ts_updated.timetuple()) * 1000
+        }                                           # Timestamp in epoch milis
 
-    return jsonify({'response': response})
+    except Exception:
+        resp['result'] = None
+
+    return jsonify(**resp)
