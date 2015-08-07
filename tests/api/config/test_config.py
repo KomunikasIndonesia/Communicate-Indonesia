2d8b005bb4291a9ac9e3f5bd637687312bd85c7c
girikuncoro <girikuncoro@gmail.com>
2015-08-06 22:30:43 -0400

    Tested update and get config

---
+++ tests/api/config/test_config.py
@@ -0,0 +1,162 @@
+import unittest
+from app.api.config import app
+from app.model.config import Config
+from google.appengine.ext import ndb, testbed
+import json
+from base64 import b64encode
+
+
+class UserTest(unittest.TestCase):
+
+    def setUp(self):
+        self.ADMIN = 'admin'
+        self.APIKEY = '123456789'
+
+        self.app = app.test_client()
+
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+
+        new = Config(admin_username=self.ADMIN,
+                     admin_apikey=self.APIKEY)
+        new.put()
+
+        ndb.get_context().clear_cache()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def headers(self, username=None, apikey=None):
+        username = username or self.ADMIN
+        apikey = apikey or self.APIKEY
+
+        return {
+            'Authorization':
+                'Basic ' + b64encode("{}:{}".format(username, apikey))
+        }
+
+    def config(self, **kwargs):
+        headers = self.headers()
+        return self.app.get('/v1/config', query_string=kwargs, headers=headers)
+
+    def config_without_auth(self, **kwargs):
+        return self.app.get('/v1/config', query_string=kwargs)
+
+    def config_with_invalid_admin(self, **kwargs):
+        headers = self.headers(username='billjobs')
+        return self.app.get('/v1/config', query_string=kwargs, headers=headers)
+
+    def config_with_invalid_apikey(self, **kwargs):
+        headers = self.headers('admin', '99999')
+        return self.app.get('/v1/config', query_string=kwargs, headers=headers)
+
+    def test_get_config(self):
+        res = self.config()
+        data = json.loads(res.data)
+
+        self.assertEqual('admin', data['admin_username'])
+        self.assertEqual('123456789', data['admin_apikey'])
+
+    def test_get_config_without_auth(self):
+        res = self.config_without_auth()
+        data = json.loads(res.data)
+
+        self.assertEqual(400, res.status_code)
+        self.assertEqual('unauthorized access', data['error'])
+
+    def test_get_config_with_invalid_admin(self):
+        res = self.config_with_invalid_admin()
+        data = json.loads(res.data)
+
+        self.assertEqual(400, res.status_code)
+        self.assertEqual('unauthorized access', data['error'])
+
+    def test_get_config_with_invalid_apikey(self):
+        res = self.config_with_invalid_apikey()
+        data = json.loads(res.data)
+
+        self.assertEqual(400, res.status_code)
+        self.assertEqual('unauthorized access', data['error'])
+
+    def test_update_config(self):
+        res = self.config(update='true',
+                          admin_username='kat',
+                          admin_apikey='123')
+        data = json.loads(res.data)
+
+        self.assertEqual(200, res.status_code)
+        self.assertEqual('kat', data['admin_username'])
+        self.assertEqual('123', data['admin_apikey'])
+
+        self.assertEqual(1, len(Config.query().fetch()))
+
+    def test_update_config_invalid(self):
+        # without update='true'
+        res = self.config(admin_username='kat',
+                          admin_apikey='123')
+        data = json.loads(res.data)
+
+        self.assertEqual(200, res.status_code)
+        self.assertEqual('admin', data['admin_username'])
+        self.assertEqual('123456789', data['admin_apikey'])
+
+        self.assertEqual(1, len(Config.query().fetch()))
+
+    def test_update_config_without_username(self):
+        res = self.config(update='true',
+                          admin_apikey='123')
+        data = json.loads(res.data)
+
+        self.assertEqual(400, res.status_code)
+        self.assertEqual('missing required parameters', data['error'])
+
+        self.assertEqual(1, len(Config.query().fetch()))
+
+    def test_update_config_without_apikey(self):
+        res = self.config(update='true',
+                          admin_username='kat')
+        data = json.loads(res.data)
+
+        self.assertEqual(400, res.status_code)
+        self.assertEqual('missing required parameters', data['error'])
+
+        self.assertEqual(1, len(Config.query().fetch()))
+
+    def test_update_without_auth(self):
+        res = self.config_without_auth(update='true',
+                                       admin_username='kat',
+                                       admin_apikey='123')
+        data = json.loads(res.data)
+
+        self.assertEqual(400, res.status_code)
+        self.assertEqual('unauthorized access', data['error'])
+
+        self.assertEqual(1, len(Config.query().fetch()))
+
+    def test_update_with_invalid_admin(self):
+        res = self.config_with_invalid_admin(update='true',
+                                             admin_username='kat',
+                                             admin_apikey='123')
+        data = json.loads(res.data)
+
+        self.assertEqual(400, res.status_code)
+        self.assertEqual('unauthorized access', data['error'])
+
+        self.assertEqual(1, len(Config.query().fetch()))
+
+    def test_update_without_auth(self):
+        res = self.config_with_invalid_apikey(update='true',
+                                              admin_username='kat',
+                                              admin_apikey='123')
+        data = json.loads(res.data)
+
+        self.assertEqual(400, res.status_code)
+        self.assertEqual('unauthorized access', data['error'])
+
+        self.assertEqual(1, len(Config.query().fetch()))
+
+
+if __name__ == '__main__':
+    unittest.main()
