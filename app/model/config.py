637c06181bd30b33c633045b88f2755287171e16
girikuncoro <girikuncoro@gmail.com>
2015-08-06 22:40:20 -0400

    Removed delete config to the model

--- app/model/config.py
+++ app/model/config.py
@@ -5,3 +5,7 @@ class Config(ndb.Model):
 
     admin_username = ndb.StringProperty()
     admin_apikey = ndb.StringProperty()
+
+
+def delete_config():
+    ndb.delete_multi(Config.query().fetch(keys_only=True))
