074b56cfead25f8f11c814d597267a7725e0b30f
girikuncoro <girikuncoro@gmail.com>
2015-08-06 21:58:49 -0400

    Added config edit from url by admin

---
+++ app/api/config/main.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, abort
+from app.model.config import Config
+from google.appengine.ext import ndb
+from app.util.flask_common import (
+    jsonify,
+    enable_json_error,
+    ensure_param,
+    require_apikey
+)
+
+
+app = Flask(__name__)
+enable_json_error(app)
+
+
+@app.route('/v1/config', methods=['GET'])
+@require_apikey
+@jsonify
+def config():
+    update = request.args.get('update')
+
+    if update == 'true':
+        admin_username = request.args.get('admin_username')
+        admin_apikey = request.args.get('admin_apikey')
+
+        if not admin_username or not admin_apikey:
+            abort(400, 'missing required parameters')
+
+        ndb.delete_multi(Config.query().fetch(keys_only=True))
+
+        new = Config(admin_username=admin_username,
+                     admin_apikey=admin_apikey)
+
+        new.put()
+
+    else:
+        config = Config.query().fetch()[0]
+        admin_username = config.admin_username
+        admin_apikey = config.admin_apikey
+
+    return {
+        'admin_username': admin_username,
+        'admin_apikey': admin_apikey
+    }
