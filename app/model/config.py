d6f5e08bf64fa9398eb072bed36ddf6eb073a59d
girikuncoro <girikuncoro@gmail.com>
2015-08-07 23:06:40 -0400

    Removed basic auth for config and required key

--- app/model/config.py
+++ app/model/config.py
@@ -5,7 +5,3 @@ class Config(ndb.Model):
 
     admin_username = ndb.StringProperty()
     admin_apikey = ndb.StringProperty()
-
-
-def delete_config():
-    ndb.delete_multi(Config.query().fetch(keys_only=True))
