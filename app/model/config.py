9bb3ba41dbf695783bea99d87302128ee7369b50
Ragil Prasetya <praser05@gmail.com>
2015-08-09 13:51:55 -0700

    Fix config endpoint to account for first config update

--- app/model/config.py
+++ app/model/config.py
@@ -5,3 +5,9 @@ class Config(ndb.Model):
 
     admin_username = ndb.StringProperty()
     admin_apikey = ndb.StringProperty()
+
+    def toJson(self):
+        return {
+            'admin_username': self.admin_username,
+            'admin_apikey': self.admin_apikey
+        }
