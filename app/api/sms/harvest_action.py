3bfc9b7d00a3bc04bbed2a4868ef87011f4d6e18
girikuncoro <girikuncoro@gmail.com>
2015-08-09 23:26:24 -0400

    Fix harvest action command: removed district_id, exception, return, aligned with dispatcher

--- app/api/sms/harvest_action.py
+++ app/api/sms/harvest_action.py
@@ -1,7 +1,6 @@
 from app.api.sms.base_action import ThreeArgCommand
 from app.command.base import Action
 from app.model.farm import Farm
-from app.model.user import User
 
 
 class HarvestAction(Action):
@@ -12,23 +11,15 @@ class HarvestAction(Action):
 
     def __init__(self, command):
         super(HarvestAction, self).__init__(command)
-        self.harvest = command.harvest
-        self.amount = command.amount
-        self.district_id = command.district()
-        self.valid = command.valid()
 
     def execute(self):
-        if not self.valid:
-            raise Exception('harvest command is invalid')
-
-        new = Farm(id=Farm.id(),
-                   district_id=self.district_id,
-                   action=self.CMD,
-                   crop_name=self.harvest,
-                   quantity=self.amount)
-        new.put()
-
-        return new.toJson()
+        for user in self.command.sms.user:
+            new = Farm(id=Farm.id(),
+                       district_id=user.district_id,
+                       action=self.CMD,
+                       crop_name=self.command.harvest,
+                       quantity=self.command.amount)
+            new.put()
 
 
 class HarvestCommand(ThreeArgCommand):
@@ -64,7 +55,3 @@ class HarvestCommand(ThreeArgCommand):
     def valid(self):
         valid_cmd = any([self.cmd == cmd for cmd in self.VALID_CMDS])
         return valid_cmd and self.amount and self.harvest
-
-    def district(self):
-        user = User.query(User.phone_number == self.sms.from_number).get()
-        return user.district_id
--- tests/api/sms/test_harvest_command.py
+++ tests/api/sms/test_harvest_command.py
@@ -1,6 +1,8 @@
 import unittest
+from mock import patch
 
 from app.api.sms.harvest_action import HarvestCommand, HarvestAction
+from app.api.sms import app
 from app.model.sms_request import SmsRequest
 from app.model.user import User
 from app.model.farm import Farm
@@ -10,6 +12,8 @@ from google.appengine.ext import testbed
 class HarvestCommandTest(unittest.TestCase):
 
     def setUp(self):
+        self.app = app.test_client()
+
         self.sms = SmsRequest()
         self.sms.from_number = '6072809193'
 
@@ -72,13 +76,10 @@ class HarvestCommandTest(unittest.TestCase):
             cmd = HarvestCommand(self.sms)
             self.assertFalse(cmd.valid())
 
-    def test_district_id_from_harvest(self):
-        self.sms.body = 'harvest 20 potato'
+    @patch('app.api.sms.main.dispatcher')
+    def test_harvest_action(self, mock):
+        mock.dispatch.return_value = None
 
-        cmd = HarvestCommand(self.sms)
-        self.assertEqual('sul123', cmd.district())
-
-    def test_harvest_action(self):
         valid_messages = [
             'harvest 20 potato',
             'harvest potato 20',
@@ -87,33 +88,25 @@ class HarvestCommandTest(unittest.TestCase):
         ]
 
         for body in valid_messages:
-            self.sms.body = body
-            cmd = HarvestCommand(self.sms)
-            act = HarvestAction(cmd).execute()
+            self.app.post('/v1/sms/twilio', data={
+                'MessageSid': 'sid',
+                'From': self.user.phone_number,
+                'To': '+321',
+                'Body': body
+            })
 
-            self.assertEqual('harvest', act['action'])
-            self.assertEqual('potato', act['crop_name'])
-            self.assertEqual(20, act['quantity'])
-            self.assertEqual('sul123', act['district_id'])
+        all_sms = SmsRequest.query().fetch()
+        self.assertEqual(4, len(all_sms))
 
-        self.assertEqual(4, len(Farm.query().fetch()))
+        for sms in all_sms:
+            cmd = HarvestCommand(sms)
+            HarvestAction(cmd).execute()
 
-    def test_harvest_action_with_invalid_command(self):
-        valid_messages = [
-            'sell 20 potato',
-            'sell potato 20',
-            'jual 20 potato',
-            'jual potato 20'
-        ]
+        all_data = Farm.query().fetch()
+        self.assertEqual(4, len(all_data))
 
-        for body in valid_messages:
-            self.sms.body = body
-            cmd = HarvestCommand(self.sms)
-
-            try:
-                HarvestAction(cmd).execute()
-                self.assertFail()
-            except Exception as e:
-                self.assertEqual('harvest command is invalid', e.message)
-
-        self.assertEqual(0, len(Farm.query().fetch()))
+        for data in all_data:
+            self.assertEqual('harvest', data.action)
+            self.assertEqual('potato', data.crop_name)
+            self.assertEqual(20, data.quantity)
+            self.assertEqual('sul123', data.district_id)
